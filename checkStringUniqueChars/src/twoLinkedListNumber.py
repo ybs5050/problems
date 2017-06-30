86f13ac5fee8212c91e0cd123929e1c7cc18ef26
ybs5050 <bfid1111@gmail.com>
2017-06-30 14:58:49 -0500

    done

--- checkStringUniqueChars/src/twoLinkedListNumber.py
+++ checkStringUniqueChars/src/twoLinkedListNumber.py
@@ -31,13 +31,47 @@ class LinkedList:
             a = a.next
         return int(num)
 
+def addLists(l1, l2, carry):
+
+    # Check final digit 
+    if l1 is None and l2 is None and carry is 0:
+        return None
+
+    # Store value and add l1 and l2
+    value = carry
+
+    if l1:
+        value += l1.data
+    if l2:
+        value += l2.data
+
+    result = Node(value % 10)
+
+    result.next = addLists(None if l1 is None else l1.next,
+                    None if l2  is None else l2.next,
+                    1 if value >= 10 else 0)
+    return result
+
+
 def main():
 
     a = LinkedList()
-    a.add(7)
-    a.add(1)
+    a.add(9)
+    a.add(3)
     a.add(6)
-    print(a.convert())
+    a.prints()
+    print('====================================')
+    b = LinkedList()
+    b.add(5)
+    b.add(6)
+    b.add(7)
+    b.prints()
+    print('====================================')
+    bb = addLists(a.head,b.head,0)
+    cc = bb
+    while(cc):
+        print(cc.data)
+        cc = cc.next
 
 if __name__ == '__main__':
     main()
